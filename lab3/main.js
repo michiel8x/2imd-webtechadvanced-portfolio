b06d806b23827bff76f0abc4afcb341d14cd06ef
michiel8x <michiel2912@gmail.com>
2022-03-03 11:53:04 +0100

    feat: add eventlisteners

--- lab3/main.js
+++ lab3/main.js
@@ -1,6 +1,5 @@
 import './style.css'
+import App from "./classes/App";
+const app = new App();
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
